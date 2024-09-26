74fe9beda08aa99a4fd3b441789c38ae41989d63
Shraddha N Bandekar <shraddha.bandekar@gmail.com>
2024-09-26 19:10:31 +0530

    should support delimiters of any length

--- calculator.test.js
+++ calculator.test.js
@@ -38,6 +38,13 @@ describe('StringCalculator', () => {
     expect(add("1,2,3,4")).toBe(10);
   });
 
+  test('should support delimiters of any length', () => {
+    expect(add("//***\n1***2***3")).toBe(6);
+     expect(add("//;;;\n1;;;2;;;3")).toBe(6);
+     expect(add("//|\n1|2|3")).toBe(6);
+   });
+ 
+
 
 
 });
