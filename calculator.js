6dbaea69afb39f45af837fa1e073f8978cabbcb5
Shraddha N Bandekar <shraddha.bandekar@gmail.com>
2024-11-06 17:53:38 +0530

    calculator assign

---
+++ calculator.js
@@ -0,0 +1,30 @@
+ function add(numbers) {
+
+
+    if(!numbers || numbers == "") return 0;
+
+    let delimiter = /,|\n/;
+    if(numbers.startsWith("//")){
+
+        const delimit = numbers.match(/^\/\/(.+)\n/);
+        const escapedDelimiter = delimit[1].replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+        delimiter = new RegExp(escapedDelimiter, "g");
+        numbers = numbers.slice(delimit[0].length);
+    }
+
+    const numArray = numbers.split(delimiter).map(Number);
+    const negativeNumbers = numArray.filter(num => num < 0);
+    if(negativeNumbers.length){
+        //console.log(`Negative numbers not allowed : ${negativeNumbers.join(", ")}`);
+        throw new Error(`Negative numbers not allowed : ${negativeNumbers.join(", ")}`) 
+    }
+
+    const filteredNumArray = numArray.filter(num => num <= 1000);
+    //console.log("filteredNumArray",filteredNumArray);
+
+    return filteredNumArray.reduce((sum,num) => sum + num, 0);
+
+
+};
+
+module.exports = add;
---
+++ calculator.test.js
@@ -0,0 +1,53 @@
+const add = require('./calculator');
+
+describe('StringCalculator', () => {
+
+  test('should return 0 for an empty string', () => {
+    expect(add("")).toBe(0);
+  });
+
+  test('should return the number itself if only one number is provided', () => {
+    expect(add("1")).toBe(1);
+  });
+
+  test('should return the sum of two numbers', () => {
+    expect(add("1,2")).toBe(3);
+  });
+
+  test('should return the sum of multiple numbers', () => {
+    expect(add("1,2,3,4")).toBe(10);
+  });
+
+  test('should handle newlines between numbers', () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
+
+  test('should handle newlines between numbers 15', () => {
+    expect(add("1\n2\n3\n4,5")).toBe(15);
+  });
+
+  test('should support different delimiters', () => {
+    expect(add("//;\n1;2")).toBe(3);
+  });
+  test('should support different delimiters ,', () => {
+    expect(add("//,\n1,2")).toBe(3);
+  });
+
+  test('should throw an exception for negative numbers', () => {
+    expect(() => add("1,-2,3")).toThrow("Negative numbers not allowed : -2");
+  });
+
+  test('should ignore numbers greater then 1000', () => {
+    expect(add("2,1001,1")).toBe(3);
+  });
+
+  test('should return the sum of multiple numbers', () => {
+    expect(add("1,2,3,4")).toBe(10);
+  });
+
+  test('should support delimiters of any length', () => {
+   expect(add("//***\n1***2***3")).toBe(6);
+    expect(add("//;;;\n1;;;2;;;3")).toBe(6);
+    expect(add("//|\n1|2|3")).toBe(6);
+  });
+});
